2da99da112d5b810a915de1df687f5c266e977cf
Lupita <lupitarivera8899@gmail.com>
2021-12-28 13:17:04 -0600

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import {Routes, route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import React from 'react';
 import './App.css';
 import About from './components/About';
 import Navigation from './components/Navigation';
+import Intro from './components/Intro';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
