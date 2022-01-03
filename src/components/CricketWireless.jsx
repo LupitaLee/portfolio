83c1655bd2aa196e170dea19b725befb4a24deff
Lupita <lupitarivera8899@gmail.com>
2022-01-03 08:29:49 -0600

    add work infor to the Cricket wireless component

--- src/components/CricketWireless.jsx
+++ src/components/CricketWireless.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const CricketWireless = () => {
     return (
-        <div>
-            
-        </div>
+    <div id="cricket-wireless">
+        <h3> CRICKET WIRELESS</h3>
+
+        <ul>
+            <li>Store opening and closing procedures including cash in and cash out.</li>  
+            <li> Participates with near businesses for partnerships and events to increase sales.</li>
+            <li>Achieve monthly sales goals constantly.</li> 
+            <li>Assemble Promotional Displays to increase sales.</li> 
+            <li>Review Inventory of phones and Accessories.</li> 
+                    
+        </ul>
+       
+    </div>
     )
 }
 
