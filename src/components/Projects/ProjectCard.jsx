4e5f2d0e9b04b298254d60201a244ff3e69236e2
Lupita <lupitarivera8899@gmail.com>
2022-01-02 22:57:15 -0600

    add first project to Project component

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Card, } from 'react-bootstrap'
+import { Card, Link } from 'react-bootstrap'
 
 const ProjectCard = () => {
     return (
         <div>
             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Link href='https://github.com/LupitaLee/dallas_amigos_frontend' >Card Link</Card.Link>
+
+            <Card.Img variant="top" src="https://media.istockphoto.com/photos/dallas-at-dusk-picture-id522580445?b=1&k=20&m=522580445&s=170667a&w=0&h=OxztE0otc9tFoQ2UZNPB5-NzgCra8oGLp0qVjwE-7Bk=" href='https://github.com/LupitaLee/dallas_amigos_frontend' />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>DALLAS AMIGOS </Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                React / Redux
                 </Card.Text>
                 {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
