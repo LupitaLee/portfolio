aa14db93be4279c2daa370f681a888957c4f1d73
Lupita <lupitarivera8899@gmail.com>
2022-01-03 10:34:08 -0600

    add project card for the CLI project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,9 +19,11 @@ const Projects = () => {
 
                 <ProjectCard title='Food Court Online' path='https://github.com/LupitaLee/FoodCourtOnline' src='https://images.unsplash.com/photo-1593768128575-e41545e803d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1hbGwlMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='front side of a restaurant with tables and people eating' description='Order Online Food Mall Courts' label='Vanilla Javascript'/>
 
-                <ProjectCard title='' path='' src='' alt='' description='' label=''/>
+                <ProjectCard title='MY PHOTOS' path='https://github.com/LupitaLee/myphotos' src='https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='lake a harm holding a camera' description='Portfolio Creator to add your personal photos using CRUD functionalities' label='Sinatra '/>
 
-                <ProjectCard title='MY PHOTOS' path='https://github.com/LupitaLee/myphotos' src='https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='lake a harm holding a camera' description='Template Design and Inspiration' label='Ruby on Rails '/>
+
+                <ProjectCard title='QUOTES' path='https://github.com/LupitaLee/quotes' src='https://images.unsplash.com/photo-1501747315-124a0eaca060?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHF1b3Rlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt='table with a coffe mug with a quote note' description='Through the Comand Line dysplay Quotes' label='CLI '/>
+
             
         </div>
     )
