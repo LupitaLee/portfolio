9801ae0e70fc4342afd2465e1fd769ebca8ece1d
Lupita <lupitarivera8899@gmail.com>
2022-01-03 08:36:21 -0600

    add work info to Motherboard component

--- src/components/Motherboard.jsx
+++ src/components/Motherboard.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Motherboard = () => {
     return (
-        <div>
+        <div id="motherboard">
+            <h3>SMS INFO CORPORATION</h3>
+
+            <ul>
+
+                <li>Troubleshoot Server Hardware including, hard disks, memory, fan, motherboard, power supply.</li> 
+                <li>Collects data and enters inspection results in a database or software program.</li> 
+                <li>Check/verify testers to required standards with test blocks provided and records data.</li> 
+                <li>Creating and applying bar code labels to finished products.</li> 
+
+
+            </ul>
             
         </div>
     )
