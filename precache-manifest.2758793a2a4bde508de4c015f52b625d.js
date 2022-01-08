b00716eb554f9e4501daaa16deb8f770fd15d6c9
Lupita <lupitarivera8899@gmail.com>
2022-01-07 22:45:30 -0600

    Updates

---
+++ precache-manifest.2758793a2a4bde508de4c015f52b625d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "179816bd42522943c9a8",
+    "url": "/portfolio/static/css/main.373d9581.chunk.css"
+  },
+  {
+    "revision": "179816bd42522943c9a8",
+    "url": "/portfolio/static/js/main.179816bd.chunk.js"
+  },
+  {
+    "revision": "5a1015f45b63e5afa53f",
+    "url": "/portfolio/static/js/1.5a1015f4.chunk.js"
+  },
+  {
+    "revision": "0eb066cee3b83d5d42fb",
+    "url": "/portfolio/static/css/2.6eef247c.chunk.css"
+  },
+  {
+    "revision": "0eb066cee3b83d5d42fb",
+    "url": "/portfolio/static/js/2.0eb066ce.chunk.js"
+  },
+  {
+    "revision": "3762eb9948b03feb942f",
+    "url": "/portfolio/static/js/runtime~main.3762eb99.js"
+  },
+  {
+    "revision": "56e12a323a236b3ff820f5e0db08c6b5",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9398d1a9fe34b2dccdce62a2dcf82830.js"
+  "/portfolio/precache-manifest.2758793a2a4bde508de4c015f52b625d.js"
 );
 
 workbox.clientsClaim();
