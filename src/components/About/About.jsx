880ab57931bc0212f5c40f7212beb9c5a9bbc817
Lupita <lupitarivera8899@gmail.com>
2022-02-06 00:24:43 -0600

    add more to about me p tags and add Mystack in blue

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,16 +15,28 @@ const About = () => {
                 <div id='about-text'>
 
                     <p>
-                    As a Full Stack Web Developer, I thrive in a dynamic environment and am confident in my decision-making ability. A quick learner, creative thinker, and problem-solver, I’m experienced with:
+                    As a Full Stack Web Developer, I thrive in a dynamic environment and am confident in my decision-making ability. A quick learner, creative thinker, and problem-solver.
                     </p>
-                    <p>
                     
-                    When I’m not coding, I’m learning the Korean Language, and supporting the family business.
+                     <p>
+                     I'm still enthusiastically grabbing onto any other programming languages, frameworks, or principles I can integrate into the coding web in my head.
+                     </p>
+                     <p>
+
+                    Yet we never finish learning and there’s certainly more to learn, more problems to solve, and more to build. And for this, I am very grateful.
+                    When I’m not coding, I’m learning the Korean Language, reading books and enjoying the nice weather.
                     </p>
+                    <br/>
+
+                    <div className="col__2">
+                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEDGWt1pHkDMA/profile-displayphoto-shrink_800_800/0/1642392504481?e=1648080000&v=beta&t=Uv9QieBNen8YST5cSgfE2DJ9NYJPlydGGKUAQdEYWmM" alt="lupita Rivera"  />
+            </div>   <br/>  
+
+
 
                     <div className='title-second'>
-                <h3><span style={{ color: '#71D4EF' }}>&gt;</span> Skills</h3>
-            </div>
+                <h3 style={{ color: '#71D4EF' }}> My Stack</h3>
+            </div> 
 
                     <ul>
                         <li>{bullet}<br/>
@@ -56,9 +68,7 @@ const About = () => {
                
             
             </div>
-            <div className="col__2">
-                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEDGWt1pHkDMA/profile-displayphoto-shrink_800_800/0/1642392504481?e=1648080000&v=beta&t=Uv9QieBNen8YST5cSgfE2DJ9NYJPlydGGKUAQdEYWmM" alt="lupita Rivera"  />
-            </div>
+           
 
         </div>
     )
